5f0589cbf866c38971a19b90efbbf6146883a585
DESKTOP-C41AGCJ\Park <sungjoonpark3@gmail.com>
2018-07-08 02:16:56 +0900

    (3일차)_Create_react_app사용법, input 만들어서 값 넣어서 표현하기

---
+++ 3일차/contact-app/src/components/PhoneForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+state={
+    name: '',
+    phone:'',
+}
+handleChange=(e)=>{
+    this.setState({
+        [e.target.name] : e.target.value
+    });
+}
+    render() {
+        return (
+            <form>
+           <input name="name"
+           placeholder="이름"
+           onChange={this.handleChange} 
+           value={this.state.name}/>
+
+           <input placeholder="전화번호"
+           name="phone"
+           onChange={this.handleChange}
+           value={this.state.phone}/>
+          <div>
+              {this.state.name} {this.state.phone}
+            </div>
+            </form>
+        );
+    }
+}
+
+export default PhoneForm;
