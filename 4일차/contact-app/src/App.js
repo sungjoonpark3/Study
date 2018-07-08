d05ebf62d588588c0dc52ee01964d111875c64ea
DESKTOP-C41AGCJ\Park <sungjoonpark3@gmail.com>
2018-07-09 00:03:55 +0900

    (4일차)_배열에 데이터 삽입하기 공부

---
+++ 4일차/contact-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+
+class App extends Component {
+  id=0;
+  state={
+    information:[],
+  }
+  handleCreate=(data)=>{
+    const{information}=this.state;
+    this.setState({
+      information: information.concat({
+
+        ...data,
+        id:this.id++
+      })
+    });
+  }
+  render() {
+    return (
+      <div>
+      <PhoneForm onCreate={this.handleCreate}/>
+      {JSON.stringify(this.state.information)}
+      </div>    
+    );
+  }
+}
+
+export default App;
