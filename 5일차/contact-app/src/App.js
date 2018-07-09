eeb1c2b4b5bf7004eeca7a8e84c022b3cd10147c
DESKTOP-C41AGCJ\Park <sungjoonpark3@gmail.com>
2018-07-09 22:47:07 +0900

    (5일차).slice사용법과 .filter의 사용을 통한 데이터 제거 하는 방법. 배열 데이터 렌더링 하는 방법

---
+++ 5일차/contact-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
+
+class App extends Component {
+  id=0;
+  state={
+    information:[],
+  }
+  handleCreate=(data)=>{
+    const{information}=this.state;
+    this.setState({
+      information: information.concat({
+
+        ...data,
+        id:this.id++
+      })
+    });
+  }
+
+  handleRemove=(id)=>{
+    const{information}=this.state;
+    this.setState({
+      information: information.filter(info=>info.id!==id)
+    });
+  }
+  render() {
+    return (
+      <div>
+      <PhoneForm onCreate={this.handleCreate}/>
+      <PhoneInfoList 
+      data={this.state.information}
+      onRemove={this.handleRemove}
+      />
+      </div>    
+    );
+  }
+}
+
+export default App;
