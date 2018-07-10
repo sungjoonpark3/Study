24e7bad8941fdc04661bbb654938260b9c0f6c8d
DESKTOP-C41AGCJ\Park <sungjoonpark3@gmail.com>
2018-07-10 23:53:56 +0900

    (7일)마지막 수정,삭제,검색,ref기능 공부

---
+++ 7일차_마지막강의/contact-app/src/components/PhoneForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+input=React.createRef();
+  state = {
+    name: '',
+    phone: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onCreate(this.state);
+    this.setState({
+      name: '',
+      phone: '',
+    });
+    this.input.current.focus();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="name"
+          placeholder="이름"
+          onChange={this.handleChange}
+          value={this.state.name}
+          //ref={ref=>this.input=ref}
+          //위의 input 값이 널일때
+          ref={this.input}
+        />
+        <input 
+          name="phone"
+          placeholder="전화번호"
+          onChange={this.handleChange}
+          value={this.state.phone}
+        />
+        <button type="submit">등록</button>
+      </form>
+    );
+  }
+}
+
+export default PhoneForm; 
+//APP선언했던 create를 호출한다.프롭스값으로
+    //페이지가 리로딩 되는 것을 막아준다. 등록 버튼을 누르게되면 리로딩 되는 것 때문에 이렇게 한다.
+    //월래해야 하는 작업을 prevnetDefault()가 막아준다.
